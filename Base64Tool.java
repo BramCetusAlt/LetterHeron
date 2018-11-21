8e3c161d020623640967b6db1e70af25585bddc4
TheLurk <39456857+BramCetusAlt@users.noreply.github.com>
2018-11-21 18:54:27 +0200

    Update Base64Tool.java

--- Base64Tool.java
+++ Base64Tool.java
@@ -1,14 +1,43 @@
+import java.util.Scanner;
 import java.util.Base64;
 
 public class Base64Tool
 {
-	public static String encrypt(String txt)
+	public void getInput(String text)
 	{
+		// Gets the data required to process the text. That includes the choice to either encrypt or decrypt the text.
+	
+		Scanner scan = new Scanner(System.in);
+		int choice;
+		String finalText;
+		System.out.println("[+] 1. Encode the text \n");
+		System.out.println("[+] 2. Decode the text \n");
+		choice = scan.nextInt();
+		
+		switch(choice)
+		{
+			case 1:
+			finalText = encrypt(text);
+			System.out.println(finalText);
+			break;
+			
+			case 2:
+			finalText = decrypt(text);
+			System.out.println(finalText);
+			break;
+		}
+	}
+	
+	public String encrypt(String txt)
+	{
+		//Encrypt the text and return the result.
 		byte[] encoded = Base64.getEncoder().encode(txt.getBytes());
 		return new String(encoded);
 	}
-	public static String decrypt(String txt)
+	
+	public String decrypt(String txt)
 	{
+		//Decrypt the text and return the result.
 		byte[] text = txt.getBytes();
 		byte[] decode = Base64.getDecoder().decode(text);
 		return new String(decode);
